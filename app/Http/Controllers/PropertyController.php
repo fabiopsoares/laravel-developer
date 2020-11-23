68519018931ee1c35a026ee824674325aa1131b8
Fabio Pereira Soares <fabiotecninf@gmail.com>
2020-11-23 14:57:58 -0300

    segundo commit

---
+++ app/Http/Controllers/PropertyController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace LaraDev\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PropertyController extends Controller
+{
+    public function index()
+    {
+        $properties = DB::select('SELECT * FROM properties');
+        /*return view ('property.index', [
+            'properties' => $properties
+        ]);*/
+
+        return view('property.index')->with('properties', $properties);
+    }
+
+    public function show($name)
+    {
+        $property = DB::select("SELECT * FROM properties WHERE name = ?", [$name]);
+
+        if (!empty($property)) {
+            return view('property.show', ['property' => $property]);
+        } else {
+            return redirect()->action('PropertyController@index');
+        }
+    }
+
+    public function create()
+    {
+        return view('property.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $propertySlug = $this->setName($request->title);
+
+        $properties = [
+            $request->title,
+            $propertySlug,
+            $request->description,
+            $request->rental_price,
+            $request->sale_price
+        ];
+        DB::insert("INSERT INTO properties(title,name,description,rental_price,sale_price) VALUES(?,?,?,?,?)", $properties);
+
+        return redirect()->action('PropertyController@index');
+    }
+
+    public function edit($name)
+    {
+
+        $property = DB::select("SELECT * FROM properties WHERE name = ?", [$name]);
+
+        if (!empty($property)) {
+            return view('property.edit', ['property' => $property]);
+        } else {
+            return redirect()->action('PropertyController@index');
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $propertySlug = $this->setName($request->title);
+
+        $properties = [
+            $request->title,
+            $propertySlug,
+            $request->description,
+            $request->rental_price,
+            $request->sale_price,
+            $id
+        ];
+        DB::update ("UPDATE properties SET title = ?, name = ?, description = ?, rental_price = ?, sale_price = ? WHERE id = ?", $properties);
+
+        return redirect()->action('PropertyController@index');
+
+    }
+
+    public function destroy($name){
+
+        $property = DB::select("SELECT * FROM properties WHERE name = ?", [$name]);
+
+        if (!empty($property)) {
+            DB::delete("DELETE FROM properties WHERE name = ?",[$name]);
+        }
+
+        return redirect()->action('PropertyController@index');
+    }
+
+    private function setName($title)
+    {
+        $propertySlug = str_slug($title);
+
+        $properties = DB::select("SELECT * FROM properties");
+        $t = 0;
+        foreach ($properties as $property) {
+            if (str_slug($property->title) === $propertySlug) {
+                $t++;
+            }
+        }
+
+        if($t> 0){
+            $propertySlug = $propertySlug . '-'.$t;
+        }
+
+        return $propertySlug;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/imoveis','PropertyController@index');
+
+Route::get('/imoveis/novo','PropertyController@create');
+Route::post('/imoveis/store','PropertyController@store');
+
+Route::get('/imoveis/{name}','PropertyController@show');
+
+Route::get('/imoveis/editar/{name}','PropertyController@edit');
+Route::put('/imoveis/update/{id}','PropertyController@update');
+
+Route::get('/imoveis/remover/{nome}','PropertyController@destroy');
